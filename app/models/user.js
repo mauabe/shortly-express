ad8e0438dab3b808bac21fcf7be7958ce213c881
Mauricio Feldman-Abe <mauabe@gmx.com>
2018-12-07 12:01:22 -0800

    added app andpublic files

---
+++ app/models/user.js
@@ -0,0 +1,14 @@
+var db = require('../config');
+var bcrypt = require('bcrypt-nodejs');
+var Promise = require('bluebird');
+
+var User = db.Model.extend({
+  tableName: 'users',
+
+  initialize: function() {
+    this.on('creating', function(model, attrs, options) {
+    });
+  }
+});
+
+module.exports = User;
